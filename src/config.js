dce0a3b609c4aa6ec45615303a761f5a50a6c503
Jimoh Damilola <jkdamilola@gmail.com>
2018-09-23 21:20:55 +0100

    make it better

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export default {
   GOOGLE_CLIENT_ID: '754461768195-odub50ti1js87jf0ebgkie0jedumkcpj.apps.googleusercontent.com',
   API_URI: process.env.NODE_ENV === 'production'
-    ? 'https://dogecodes-chat-api.herokuapp.com'
+    ? 'https://riotly-chat-api.herokuapp.com/'
     : 'http://localhost:8000/api/v1',
   SOCKETS_URI: process.env.NODE_ENV === 'production'
-    ? 'wss://dogecodes-chat-api.herokuapp.com'
+    ? 'wss://riotly-chat-api.herokuapp.com/'
     : 'ws://localhost:8000/api/v1',
 };
