606d3f5d1e626c6adeea5d6916ae471a29b28c71
Jimoh Damilola <jkdamilola@gmail.com>
2018-09-23 21:52:08 +0100

    make it better

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export default {
   GOOGLE_CLIENT_ID: '754461768195-odub50ti1js87jf0ebgkie0jedumkcpj.apps.googleusercontent.com',
   API_URI: process.env.NODE_ENV === 'production'
-    ? 'https://riotly-chat-api.herokuapp.com/'
-    : 'http://localhost:8000/api/v1',
+    ? 'https://riotly-chat-api.herokuapp.com/v1'
+    : 'http://localhost:8000/v1',
   SOCKETS_URI: process.env.NODE_ENV === 'production'
-    ? 'wss://riotly-chat-api.herokuapp.com/'
-    : 'ws://localhost:8000/api/v1',
+    ? 'wss://riotly-chat-api.herokuapp.com/v1'
+    : 'ws://localhost:8000/v1',
 };
